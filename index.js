fb6a6215510cff20d3024b0d44d679d8cf7f1d0e
root <root@sd-52228.dedibox.fr>
2020-06-07 01:30:09 +0200

    Remove useless finally block + wait for input to show up

--- index.js
+++ index.js
@@ -31,26 +31,31 @@ http.createServer(function(req, res) {
       try {
         console.log('Waiting for Cloudflare challenge');
         await page.waitFor(() => !document.querySelector('.ray_id'));
-        await page.waitFor('img[src="/static/img/footer.png"]', {timeout: 60000});
+        await page.waitFor('img[src="/static/img/footer.png"]', {
+          timeout: 60000
+        });
       } catch (e) {
         console.log('Error', e)
         await browser.close();
         return;
-      } finally {
-        const loginBodyHandle = await page.$('body');
-        const loginResHTML = await page.evaluate(body => body.outerHTML, loginBodyHandle);
-        await loginBodyHandle.dispose();
-        // If user is not already logged in
-        if (!/user\/logout/i.test(loginResHTML)) {
-          await page.click('#register');
-          await page.type("input[name='id']", username);
-          await page.type("input[name='pass']", password);
-          await page.keyboard.press('Enter');
-          console.log('User successfully logged-in');
-        } else {
-          console.log('User already logged-in');
-        }
       }
+
+      const loginBodyHandle = await page.$('body');
+      const loginResHTML = await page.evaluate(body => body.outerHTML, loginBodyHandle);
+      await loginBodyHandle.dispose();
+      // If user is not already logged in
+      if (!/user\/logout/i.test(loginResHTML)) {
+        await page.click('#register');
+        // Wait for login form to show up
+        await page.waitFor('input[name="id"]');
+        await page.type('input[name="id"]', username);
+        await page.type('input[name="pass"]', password);
+        await page.keyboard.press('Enter');
+        console.log('User successfully logged-in');
+      } else {
+        console.log('User already logged-in');
+      }
+
     }
 
     console.log('Fetching requested URL');
@@ -75,28 +80,29 @@ http.createServer(function(req, res) {
         console.log('Error', e)
         await browser.close();
         return;
-      } finally {
-        console.log('Downloading file');
-        const contentDisposition = finalResponse.headers()['content-disposition'];
-        const fileName = contentDisposition.slice(contentDisposition.indexOf('"') + 1, contentDisposition.length - 1);
-        const filePath = path.join(downloadPath, fileName);
-
-        var i = 0;
-        while (!fs.existsSync(filePath)) {
-          await new Promise(resolve => setTimeout(resolve, 500));
-          if (i++ > 10)
-            break;
-        }
-
-        const fileStream = fs.createReadStream(filePath);
-
-        res.writeHead(200, {
-          'Content-Type': 'application/x-bittorrent',
-          'Content-Length': fs.statSync(filePath).size,
-          'content-disposition': contentDisposition
-        });
-        fileStream.pipe(res);
       }
+
+      console.log('Downloading file');
+      const contentDisposition = finalResponse.headers()['content-disposition'];
+      const fileName = contentDisposition.slice(contentDisposition.indexOf('"') + 1, contentDisposition.length - 1);
+      const filePath = path.join(downloadPath, fileName);
+
+      var i = 0;
+      while (!fs.existsSync(filePath)) {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        if (i++ > 10)
+          break;
+      }
+
+      const fileStream = fs.createReadStream(filePath);
+
+      res.writeHead(200, {
+        'Content-Type': 'application/x-bittorrent',
+        'Content-Length': fs.statSync(filePath).size,
+        'content-disposition': contentDisposition
+      });
+      fileStream.pipe(res);
+
     } else {
       try {
         console.log('Waiting for Cloudflare challenge');
@@ -106,25 +112,26 @@ http.createServer(function(req, res) {
         console.log('Error', e)
         await browser.close();
         return;
-      } finally {
-        console.log('Extracting html');
-        const bodyHandle = await page.$('body');
-        const resHTML = await page.evaluate(body => body.outerHTML, bodyHandle);
-        await bodyHandle.dispose();
-
-        if (/rss/i.test(req.url)) {
-          res.writeHead(200, {
-            'Content-Type': 'application/rss+xml'
-          });
-        }
-        res.write(resHTML);
-        res.end();
       }
+
+      console.log('Extracting html');
+      const bodyHandle = await page.$('body');
+      const resHTML = await page.evaluate(body => body.outerHTML, bodyHandle);
+      await bodyHandle.dispose();
+
+      if (/rss/i.test(req.url)) {
+        res.writeHead(200, {
+          'Content-Type': 'application/rss+xml'
+        });
+      }
+      res.write(resHTML);
+      res.end();
+
     }
 
     await browser.close();
     console.log('Done');
   });
 }).listen(process.env.PORT || 8091, process.env.HOST || '0.0.0.0', function() {
-  console.log('RSS Server running');
+  console.log('ygg fetcher running');
 })
