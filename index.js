18fca8572c8513bf3a6881ffa97b6609c246c964
root <root@sd-52228.dedibox.fr>
2020-06-06 13:43:01 +0200

    Add try.catch + check if user is already logged in

--- index.js
+++ index.js
@@ -10,27 +10,47 @@ const username = process.env.YGG_USERNAME;
 const password = process.env.YGG_PASSWORD;
 
 puppeteer.use(StealthPlugin());
-if (!fs.existsSync(downloadPath)){
-    fs.mkdirSync(downloadPath);
+if (!fs.existsSync(downloadPath)) {
+  fs.mkdirSync(downloadPath);
 }
+
 
 http.createServer(function(req, res) {
   console.log('Incomming request for', req.url);
   console.log('Launching headless browser...');
   puppeteer.launch({
     headless: true,
-    args: [ '--no-sandbox' ]
+    args: ['--no-sandbox']
   }).then(async browser => {
+
     const page = await browser.newPage();
-    
+
     if (username != null && username != '') {
       console.log('Logging with creddentials');
       await page.goto(yggBaseUrl);
-      await page.waitFor('#register');
-      await page.click('#register');
-      await page.type("input[name='id']", username);
-      await page.type("input[name='pass']", password);
-      await page.keyboard.press('Enter');
+      try {
+        console.log('Waiting for Cloudflare challenge');
+        await page.waitFor(() => !document.querySelector('.ray_id'));
+        await page.waitFor('img[src="/static/img/footer.png"]', {timeout: 60000});
+      } catch (e) {
+        console.log('Error', e)
+        await browser.close();
+        return;
+      } finally {
+        const loginBodyHandle = await page.$('body');
+        const loginResHTML = await page.evaluate(body => body.outerHTML, loginBodyHandle);
+        await loginBodyHandle.dispose();
+        // If user is not already logged in
+        if (!/user\/logout/i.test(loginResHTML)) {
+          await page.click('#register');
+          await page.type("input[name='id']", username);
+          await page.type("input[name='pass']", password);
+          await page.keyboard.press('Enter');
+          console.log('User successfully logged-in');
+        } else {
+          console.log('User already logged-in');
+        }
+      }
     }
 
     console.log('Fetching requested URL');
@@ -38,59 +58,72 @@ http.createServer(function(req, res) {
     await page.goto(url);
 
     if (/download/i.test(req.url)) {
-      console.log("Waiting for Cloudflare challenge");
       page._client.send('Page.setDownloadBehavior', {
         behavior: 'allow',
         downloadPath: downloadPath,
       });
-      const finalResponse = await page.waitForResponse(function(response) {
-        return /download/i.test(response.url()) &&
-          response.status() === 200 &&
-          response.headers()['content-disposition'] != null;
-      });
 
-      console.log('Downloading file');
-      const contentDisposition = finalResponse.headers()['content-disposition'];
-      const fileName = contentDisposition.slice(contentDisposition.indexOf('"') + 1, contentDisposition.length - 1);
-      const filePath = path.join(downloadPath, fileName);
-      
-      var i = 0;
-      while(!fs.existsSync(filePath)) {
+      try {
+        console.log("Waiting for Cloudflare challenge");
+        const finalResponse = await page.waitForResponse(function(response) {
+          return /download/i.test(response.url()) &&
+            response.status() === 200 &&
+            response.headers()['content-disposition'] != null;
+        });
+      } catch (e) {
+        console.log('Error', e)
+        await browser.close();
+        return;
+      } finally {
+        console.log('Downloading file');
+        const contentDisposition = finalResponse.headers()['content-disposition'];
+        const fileName = contentDisposition.slice(contentDisposition.indexOf('"') + 1, contentDisposition.length - 1);
+        const filePath = path.join(downloadPath, fileName);
+
+        var i = 0;
+        while (!fs.existsSync(filePath)) {
           await new Promise(resolve => setTimeout(resolve, 500));
           if (i++ > 10)
             break;
-      }
-      
-      const fileStream = fs.createReadStream(filePath);
+        }
 
-      res.writeHead(200, {
-        'Content-Type': 'application/x-bittorrent',
-        'Content-Length': fs.statSync(filePath).size,
-        'content-disposition': contentDisposition
-      });
-      fileStream.pipe(res);
-    } else {
-      console.log('Waiting for Cloudflare challenge');
-      await page.waitFor(() => !document.querySelector('.ray_id'));
-      await page.waitForResponse(response => response.status() === 200);
-      console.log('Extracting html');
+        const fileStream = fs.createReadStream(filePath);
 
-      const bodyHandle = await page.$('body');
-      const resHTML = await page.evaluate(body => body.outerHTML, bodyHandle);
-      await bodyHandle.dispose();
-
-      if (/rss/i.test(req.url)) {
         res.writeHead(200, {
-          'Content-Type': 'application/rss+xml'
+          'Content-Type': 'application/x-bittorrent',
+          'Content-Length': fs.statSync(filePath).size,
+          'content-disposition': contentDisposition
         });
+        fileStream.pipe(res);
       }
-      res.write(resHTML);
-      res.end();
+    } else {
+      try {
+        console.log('Waiting for Cloudflare challenge');
+        await page.waitFor(() => !document.querySelector('.ray_id'));
+        await page.waitFor(/rss/i.test(req.url) ? '.line' : 'img[src="/static/img/footer.png"]');
+      } catch (e) {
+        console.log('Error', e)
+        await browser.close();
+        return;
+      } finally {
+        console.log('Extracting html');
+        const bodyHandle = await page.$('body');
+        const resHTML = await page.evaluate(body => body.outerHTML, bodyHandle);
+        await bodyHandle.dispose();
+
+        if (/rss/i.test(req.url)) {
+          res.writeHead(200, {
+            'Content-Type': 'application/rss+xml'
+          });
+        }
+        res.write(resHTML);
+        res.end();
+      }
     }
 
-    await browser.close()
+    await browser.close();
     console.log('Done');
   });
 }).listen(process.env.PORT || 8091, process.env.HOST || '0.0.0.0', function() {
   console.log('RSS Server running');
-});
+})
